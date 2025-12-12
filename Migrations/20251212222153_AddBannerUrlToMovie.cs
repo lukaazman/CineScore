1b622b2c552ad9b9a4aadedc63c9d77749ad064a
lukaazman <92147962+lukaazman@users.noreply.github.com>
2025-12-13 00:53:28 +0100

    Banner images
    Replaced the poster image with a 16:9 banner on the movie details page.

---
+++ Migrations/20251212222153_AddBannerUrlToMovie.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CineScore.Migrations
+{
+    public partial class AddBannerUrlToMovie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF COL_LENGTH('dbo.Movies', 'BannerUrl') IS NULL
+BEGIN
+    ALTER TABLE [dbo].[Movies] ADD [BannerUrl] nvarchar(max) NULL;
+END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF COL_LENGTH('dbo.Movies', 'BannerUrl') IS NOT NULL
+BEGIN
+    ALTER TABLE [dbo].[Movies] DROP COLUMN [BannerUrl];
+END");
+        }
+    }
+}
--- Models/TmdbOptions.cs
+++ Models/TmdbOptions.cs
@@ -5,6 +5,8 @@ namespace CineScore.Models
         public string ApiKey { get; set; } = string.Empty;
         public string ReadAccessToken { get; set; } = string.Empty;
         public string ImageBaseUrl { get; set; } = "https://image.tmdb.org/t/p/w500";
+        public string BannerImageBaseUrl { get; set; } = "https://image.tmdb.org/t/p/original";
+        public string BannerFallbackPosterBaseUrl { get; set; } = "https://image.tmdb.org/t/p/original";
         public int PageSize { get; set; } = 12;
         public int MaxPages { get; set; } = 5;
     }
